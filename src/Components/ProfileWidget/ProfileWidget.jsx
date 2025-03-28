2df0a352e3a19f79a5ca69a0eec5b23623e71fab
Gökay Baz <gokaybaz2000@outlook.com>
2025-03-28 14:44:36 +0300

    [Fixed] Build Problem.

---
+++ src/Components/ProfileWidget/ProfileWidget.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import './ProfileWidget.css'; // If you need styles
+
+const ProfileWidget = () => {
+  return (
+    <div className="profile-widget">
+      <h2>Profile</h2>
+      {/* Add your profile widget content here */}
+      <div className="profile-info">
+        <p>Your profile information will be displayed here</p>
+      </div>
+    </div>
+  );
+};
+
+export default ProfileWidget;
